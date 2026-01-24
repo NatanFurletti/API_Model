bd9ea0f1250135b796c82bd57d1c3676fcb7547f
Natan Furletti <natan.furletti@outlook.com>
2026-01-23 22:05:17 -0300

    git test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+function Home() {
+  return <h1>teste</h1>;
+}
+
 const { Person } = require("./person");
 
 require("./modules/express");
 
 const person = new Person("Alice", 30);
+
+export default Home;
